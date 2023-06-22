edb163e4b77f8312149fa23f1c9d49c6ff57b7d0
Eirik Ronæss Hansen <eirikrhansen@gmail.com>
2023-06-22 02:05:36 +0200

    Contact form icon imports ++

--- App/src/components/Contact.js
+++ App/src/components/Contact.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 class Contact extends Component {
     constructor(props) {
@@ -10,10 +14,49 @@ class Contact extends Component {
             message: "",
         };
     }
+    handleSendMesage(event) {
+        event.preventDefault();
+    }
+
+    handleInputFieldChange(event, target) {
+        event.preventDefault();
+    }
     render() {
         return (
             <div className="contact-container">
                 <form className="contact-form">
+                    <div className="info-text">
+                        <p>
+                            Do you have a question for me, or just want to have
+                            a chat? <br />
+                            <br />
+                            Reach me through LinkedIn, <br />
+                            Check out my GitHub, <br />
+                            or simply write me a message below.
+                        </p>
+                        <div className="contact-icons">
+                            <Link
+                                to="https://www.linkedin.com/in/eirik-ron%C3%A6ss-hansen-b3b088118/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FontAwesomeIcon
+                                    className="icon"
+                                    icon={faLinkedin}
+                                />
+                            </Link>
+                            <Link
+                                to="https://github.com/EirikRH"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FontAwesomeIcon
+                                    className="icon"
+                                    icon={faGithubSquare}
+                                />
+                            </Link>
+                        </div>
+                    </div>
                     <input
                         name="name"
                         className="text-input"
@@ -38,7 +81,12 @@ class Contact extends Component {
                         type="text-field"
                         placeholder="Message..."
                     ></textarea>
-                    <button className="send-button">Send Message</button>
+                    <button
+                        className="send-button"
+                        onClick={this.handleSendMesage.bind(this)}
+                    >
+                        Send Message
+                    </button>
                 </form>
             </div>
         );
