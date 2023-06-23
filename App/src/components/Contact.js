8595e6f58e9a66832876ff9192c54abf6088c1e4
Eirik Ronæss Hansen <eirikrhansen@gmail.com>
2023-06-23 20:15:23 +0200

    Responsive contact component & form

--- App/src/components/Contact.js
+++ App/src/components/Contact.js
@@ -24,70 +24,70 @@ class Contact extends Component {
     render() {
         return (
             <div className="contact-container">
-                <form className="contact-form">
-                    <div className="info-text">
-                        <p>
-                            Do you have a question for me, or just want to have
-                            a chat? <br />
-                            <br />
-                            Reach me through LinkedIn, <br />
-                            Check out my GitHub, <br />
-                            or simply write me a message below.
-                        </p>
-                        <div className="contact-icons">
-                            <Link
-                                to="https://www.linkedin.com/in/eirik-ron%C3%A6ss-hansen-b3b088118/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <FontAwesomeIcon
-                                    className="icon"
-                                    icon={faLinkedin}
-                                />
-                            </Link>
-                            <Link
-                                to="https://github.com/EirikRH"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <FontAwesomeIcon
-                                    className="icon"
-                                    icon={faGithubSquare}
-                                />
-                            </Link>
-                        </div>
+                <div className="contact-text">
+                    Do you have a question for me, or just want to have a chat?
+                    <br />
+                    <br />
+                    Reach me through LinkedIn, <br />
+                    Check out my GitHub, <br />
+                    or simply write me a message below.
+                    <div className="contact-icons">
+                        <Link
+                            to="https://www.linkedin.com/in/eirik-ron%C3%A6ss-hansen-b3b088118/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FontAwesomeIcon
+                                className="contact-icon"
+                                icon={faLinkedin}
+                            />
+                        </Link>
+                        <Link
+                            to="https://github.com/EirikRH"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FontAwesomeIcon
+                                className="contact-icon"
+                                icon={faGithubSquare}
+                            />
+                        </Link>
                     </div>
-                    <input
-                        name="name"
-                        className="text-input"
-                        type="text"
-                        placeholder="What do I call you?"
-                    ></input>
-                    <input
-                        name="subject"
-                        className="text-input"
-                        type="text"
-                        placeholder="Subject.."
-                    ></input>
-                    <input
-                        name="contactInfo"
-                        className="text-input"
-                        type="text"
-                        placeholder="Contact information..."
-                    ></input>
-                    <textarea
-                        name="message"
-                        className="message-input"
-                        type="text-field"
-                        placeholder="Message..."
-                    ></textarea>
-                    <button
-                        className="send-button"
-                        onClick={this.handleSendMesage.bind(this)}
-                    >
-                        Send Message
-                    </button>
-                </form>
+                </div>
+                <div className="form-container">
+                    <form className="contact-form">
+                        <input
+                            name="name"
+                            className="text-input"
+                            type="text"
+                            placeholder="What do I call you?"
+                        ></input>
+                        <input
+                            name="subject"
+                            className="text-input"
+                            type="text"
+                            placeholder="Subject.."
+                        ></input>
+                        <input
+                            name="contactInfo"
+                            className="text-input"
+                            type="text"
+                            placeholder="Contact information..."
+                        ></input>
+                        <textarea
+                            name="message"
+                            className="message-input"
+                            type="text-field"
+                            placeholder="Message..."
+                        ></textarea>
+                        <button
+                            className="send-button"
+                            onClick={this.handleSendMesage.bind(this)}
+                        >
+                            Send Message
+                        </button>
+                    </form>
+                </div>
             </div>
         );
     }
